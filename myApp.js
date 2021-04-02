5a4a9dc71d9bf1ad1ea128910e97ff28376e6c57
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-04-02 13:16:35 +0200

    changes

--- myApp.js
+++ myApp.js
@@ -1,7 +1,30 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
 
-let Person;
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('DB connected!')
+})
+.catch(err => {
+  console.error('DB connection error', err);
+})
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [{
+    type: String
+  }]
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
